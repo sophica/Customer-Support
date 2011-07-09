0fcf5f62ec7990c97083735fc20818d4f3c88acb
Sophie Zhou <sophiez@andrew.cmu.edu>
2011-07-09 04:14:52 -0700

    Created a method to populate the database and also the basic index page

--- crank/app/controllers/Application.java
+++ crank/app/controllers/Application.java
@@ -10,7 +10,29 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
+    	List<Company> companies = Company.find("order by lastUpdated desc").fetch(2);
+    	
+    	renderArgs.put("companies", companies);
         render();
+    }
+    
+    public static void populateDatabase() {
+    	Company.deleteAll();
+    	
+    	Company company1 = new Company("HP");
+    	company1.satisfaction = 50;
+    	company1.lastUpdated = new Date(10000000);
+    	company1.save();
+    	
+    	Company company2 = new Company("Dell");
+    	company2.satisfaction = 75;
+    	company2.lastUpdated = new Date(11000001);
+    	company2.save();
+    	
+    	Company company3 = new Company("Apple");
+    	company3.satisfaction = 60;
+    	company3.lastUpdated = new Date(12000002);
+    	company3.save();
     }
 
 }
