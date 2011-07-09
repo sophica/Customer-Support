bd05c70d0da959ee4a2bafbfd0909631d3f4ae5e
Sophie Zhou <sophiez@andrew.cmu.edu>
2011-07-09 03:41:07 -0700

    Added Company and Tag models

--- crank/app/models/Company.java
+++ crank/app/models/Company.java
@@ -6,6 +6,6 @@ import play.db.jpa.Model;
 
 @Entity
 public class Company extends Model{
-	
-
+	public String 	name;
+	public int 		satisfaction;
 }
---
+++ crank/app/models/Tag.java
@@ -0,0 +1,9 @@
+package models;
+
+import javax.persistence.Entity;
+import play.db.jpa.Model;
+
+@Entity
+public class Tag extends Model{
+	public String name;
+}
