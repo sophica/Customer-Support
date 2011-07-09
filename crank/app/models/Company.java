f49512ba337aa9d68a5242d062169d7f468cd049
Sophie Zhou <sophiez@andrew.cmu.edu>
2011-07-09 03:41:07 -0700

    Added Company and Tag models with manytomany links

--- crank/app/models/Company.java
+++ crank/app/models/Company.java
@@ -1,11 +1,18 @@
 package models;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 
 import play.db.jpa.Model;
 
 @Entity
 public class Company extends Model{
+	public String 	name;
+	public int 		satisfaction;
 	
-
+	@ManyToMany(cascade=CascadeType.PERSIST)
+	public Set<Tag> tags;
 }
