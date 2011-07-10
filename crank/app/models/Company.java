821ef91cb67a48e0b8ff46cbb793e30f60df07cc
Sophie Zhou <sophiez@andrew.cmu.edu>
2011-07-09 20:32:15 -0700

    Uploading Logos, styling changes

--- crank/app/models/Company.java
+++ crank/app/models/Company.java
@@ -7,18 +7,22 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 
+import play.db.jpa.Blob;
 import play.db.jpa.Model;
 
 @Entity
 public class Company extends Model{
 	public String 	name;
+	public Blob   image;
 	public int 		satisfaction;
 	public Date		lastUpdated;
+	public boolean    hasImage;
 	
 	@ManyToMany(cascade=CascadeType.PERSIST)
 	public Set<Tag> tags;
 	
 	public Company(String n) {
 		name = n;
+		hasImage = false;
 	}
 }
